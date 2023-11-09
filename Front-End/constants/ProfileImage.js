ded973e4ac45fcd834422782b9247decb9913219
Peter Yan <109417931+bustop69@users.noreply.github.com>
2023-11-09 06:45:34 +0000

    Delete Front-End directory

--- Front-End/constants/ProfileImage.js
+++
@@ -1,5 +0,0 @@
-import profile from "../assets/images/profilePic.jpg";
-
-export default {
-  profile,
-};
--- Front-End/redux/Store.js
+++
@@ -1,7 +0,0 @@
-import {createStore,combineReducers,applyMiddleware } from 'redux';
-import { Reducer } from './Reducer';
-import thunk from 'redux-thunk';
-
-const rootReducer = combineReducers({Reducer})
-
-export const Store = createStore(rootReducer,applyMiddleware(thunk))
